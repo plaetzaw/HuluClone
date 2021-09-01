9acc55f3ccb10c61d8f6f33de3c2582d4d354bb9
plaetzaw <plaetzaw@gmail.com>
2021-09-01 16:06:06 -0500

    Initial build done

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Navbar from '../components/Molecules/Navbar'
 import Header from '../components/Molecules/Header'
+import Results from '../components/Molecules/Results'
+import requests from '../utility/requests'
 
-export default function Home () {
+export default function Home ({ results }) {
   return (
     <div>
       <Head>
@@ -11,7 +13,18 @@ export default function Home () {
       </Head>
       <Header />
       <Navbar />
-      {/* //Results */}
+      <Results results={results} />
     </div>
   )
+}
+
+export async function getServerSideProps (context) {
+  const genre = context.query.genre
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending}`
+  ).then(res => res.json())
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
